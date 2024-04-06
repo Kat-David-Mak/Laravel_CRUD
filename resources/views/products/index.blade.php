2ffc1e7aadc8e1227c430ff0aa4f473c76d2f7ac
Katleho Makoetje <katlehomakoetje@gmmail.com>
2024-04-06 12:44:35 +0200

    Added Breeze with login and register

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
+    
     <h1>Products</h1>
     <div>
         @if (session()->has('success'))
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
@@ -7,9 +8,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/product', [ProductController::class, 'store'])->name('product.store');
-Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/product/{product}/deleteData', [ProductController::class, 'deleteData'])->name('product.deleteData');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
+    //default generated breeze routes
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //product routes
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/{product}/deleteData', [ProductController::class, 'deleteData'])->name('product.deleteData');
+});
+
+require __DIR__.'/auth.php';
